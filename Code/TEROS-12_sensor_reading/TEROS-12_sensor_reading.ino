c93994fffb89945d3b97eaef613221424c15aa2a
Dries Nuttin <dries.nuttin@hotmail.com>
2024-04-27 22:44:36 +0200

    Updated Teros 12 code

---
+++ Code/TEROS-12_sensor_reading/TEROS-12_sensor_reading.ino
@@ -0,0 +1,58 @@
+#include <SDI12.h>
+
+#define SDI12_DATA_PIN 5  // Pin connected to the SDI-12 data line of the sensor
+SDI12 mySDI12(SDI12_DATA_PIN);
+
+void setup() {
+  Serial.begin(9600);  // Start serial communication at 9600 baud rate
+  mySDI12.begin();     // Begin SDI-12 communication
+  Serial.println("SDI-12 sensor communication initialized");
+}
+
+void loop() {
+  // Initiate a measurement
+  String measurementCommand = "0M!";  // Change address '0' if your sensor has a different address
+  mySDI12.sendCommand(measurementCommand);
+  delay(1500);  // Wait for the sensor to process the command
+
+  // Request the measured data
+  String dataCommand = "0D0!";  // Request data from the sensor
+  mySDI12.sendCommand(dataCommand);
+  delay(300);  // Wait for the data to be ready
+
+  // Read the full response from the sensor and parse the data
+  String response = readFullResponse();
+  if (response.length() > 0) {
+    parseSensorData(response);
+  } else {
+    Serial.println("No data received. Check sensor and command syntax.");
+  }
+
+  delay(5000);  // Wait 5 seconds before the next loop iteration
+}
+
+String readFullResponse() {
+  String data = "";
+  while (mySDI12.available()) {
+    char c = mySDI12.read();
+    data += c;  // Append each character into the response string
+  }
+  return data;
+}
+
+void parseSensorData(String data) {
+  int firstPlus = data.indexOf('+', 1);  // Find the first '+' after the address '0'
+  int secondPlus = data.indexOf('+', firstPlus + 1);  // Find the second '+'
+  int endOfData = data.indexOf('+', secondPlus + 1);  // Find the end of data segment
+
+  String moisture = data.substring(firstPlus + 1, secondPlus);
+  String temperature = data.substring(secondPlus + 1, endOfData);
+  String conductivity = data.substring(endOfData + 1);
+
+  Serial.print("Moisture: ");
+  Serial.println(moisture);
+  Serial.print("Temperature: ");
+  Serial.println(temperature);
+  Serial.print("Conductivity: ");
+  Serial.println(conductivity);
+}
